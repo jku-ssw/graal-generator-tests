ce79e3e1bc88a61d7bf26f5fe1ea5db41a354e7d
skloibi <sebastian.kloibhofer@gmail.com>
2019-01-28 08:45:20 +0100

    Reduce maximum loop iterations for `ManyLoops` generative

--- projects/at.jku.ssw.java.bytecode.generator.tests/src/at/jku/ssw/java/bytecode/generator/tests/generation/classes/ManyLoops.java
+++ projects/at.jku.ssw.java.bytecode.generator.tests/src/at/jku/ssw/java/bytecode/generator/tests/generation/classes/ManyLoops.java
@@ -1,5 +1,5 @@
 package at.jku.ssw.java.bytecode.generator.tests.generation.classes;
 
-@Generative.CLIArguments(iterations = 50, args = {"-mli", "100", "-while", "50", "-for", "50", "-dowhile", "50"})
+@Generative.CLIArguments(iterations = 50, args = {"-mli", "20", "-while", "50", "-for", "50", "-dowhile", "50"})
 public class ManyLoops extends Generative {
 }
